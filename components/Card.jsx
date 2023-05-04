f4f78044f36fa21227cf5b3d0ec422aeded39a91
Abdul Basit U.S <basit258906@gmail.com>
2023-05-04 15:34:01 +0530

    complete the card component and make the result section responsive

--- components/Card.jsx
+++ components/Card.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Card = ({ result }) => {
     const BASE_URL = 'https://image.tmdb.org/t/p/original';
   return (
-    <div>
+    <div className="p-3 cursor-pointer hover:text-white active:text-red-400 lg:hover:scale-105 transition-transform duration-200">
         <Image 
             layout="responsive" 
             src={BASE_URL + result.backdrop_path} 
@@ -12,12 +12,12 @@ const Card = ({ result }) => {
             height={100}
             alt={result.title}
         />
-        <div className="">
-            <p className="truncate">{result.overview}</p>
-            <h2>{result.title || result.name}</h2>
-            <p>
+        <div className="p-2">
+            <p className="truncate text-lg">{result.overview}</p>
+            <h2 className="text-lg font-bold">{result.title || result.name}</h2>
+            <p className="flex items-center">
                 {result.release_date || result.first_air_date}
-                <HandThumbUpIcon className="h-5"/>
+                <HandThumbUpIcon className="h-5 ml-3 mr-1"/>
                 {result.vote_count}
             </p>
         </div>
