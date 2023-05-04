b490a0bf2ad93aa7eb29291cd690dfcdb39a7fd1
Abdul Basit U.S <basit258906@gmail.com>
2023-05-04 14:35:55 +0530

    create server side function and pass it to client side

--- components/Results.jsx
+++ components/Results.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const Results = () => {
+const Results = ({ results }) => {
   return (
     <div>
-        <h1>Results</h1>
+        {results.map((result, index) => (
+          <h2 key={index}>{result.title}</h2>
+        ))}
     </div>
   )
 }
